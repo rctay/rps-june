b0077385a57ed552efd720b479d83ff928a1909f
Ray <rtay@vmware.com>
2021-06-23 09:14:01 +0000

    Add test for first scenario in API

---
+++ src/main/java/com/xp/rps/PlayRequest.java
@@ -0,0 +1,11 @@
+package com.xp.rps;
+
+public class PlayRequest {
+    private final String player1;
+    private final String player2;
+
+    public PlayRequest(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+}
--- src/test/java/com/xp/rps/RpsApplicationTests.java
+++ src/test/java/com/xp/rps/RpsApplicationTests.java
@@ -1,13 +1,33 @@
 package com.xp.rps;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RpsApplicationTests {
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
     @Test
-    void contextLoads() {
+    void rockVsScissors_returnsPlayer1Wins() {
+        // arrange
+
+        // act
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                "/play",
+                new PlayRequest("ROCK", "SCISSORS"),
+                String.class);
+
+        // assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals("Player 1 Wins!", response.getBody());
     }
 
 }
